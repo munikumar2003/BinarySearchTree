7c975b2873cc09ac901ef73e5c50089486c4e921
CHEMURU MUNI KUMAR <121540522+munikumar2003@users.noreply.github.com>
2022-12-29 19:14:47 +0530

    Update and rename main.java to BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class main {
+class BinarySearchTree {
 	static class Node {
 		int key;
 		Node left, right;
@@ -11,12 +11,26 @@ class main {
 		return temp;
 	}
 	static void inorder(Node root){
-		if(root != null){
+		if(root == null)
+			return;
 			inorder(root.left);
 			System.out.print(root.key+" ");
 			inorder(root.right);
-		}
 	}
+	static void preorder(Node node){
+        if (node == null)
+            return;
+        System.out.print(node.key + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+	static void postorder(Node node){
+        if (node == null)
+            return;
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.key + " ");
+    }
 	static Node insert(Node node, int key){
 		if(node == null)
 			return newNode(key);
@@ -32,8 +46,18 @@ class main {
 			newNode = newNode.left;
 		System.out.println("\nSmallest number in the order is "+newNode.key);
 	}
-	
-	
+	static void search(Node node, int value){
+		try{
+		if(node == null)
+			System.out.println("Node not found");
+		if(value < node.key)
+			search(node.left, value);
+		else if(value > node.key) 
+			search(node.right, value);
+		if(node.key == value)
+			System.out.println("Node Found in Tree");
+		}catch(NullPointerException e){}
+	}
 	public static void main(String[] args){
 		Node root = null;
 		root = insert(root, 6);
@@ -50,6 +74,12 @@ class main {
 		root = insert(root, 2);
 		System.out.println("Inorder traversal of the given tree");
 		inorder(root);
+		System.out.println("\nPreorder traversal of the given tree");
+		preorder(root);
+		System.out.println("\nPostorder traversal of the given tree");
+		postorder(root);
 		smallnum(root);
+		search(root, 14);
+		search(root, 16);
 	}
 }
